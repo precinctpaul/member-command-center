f7d1ea8472d5ea0308b9ebd8a51d5c9a47ce7194
precinctpaul <paul@precinct.us>
2026-06-02 11:27:19 -0400

    Create people.js

---
+++ data/people.js
@@ -0,0 +1,62 @@
+window.MEMBER_COMMAND_CENTER_PEOPLE = [
+  {
+    id: "jake-auchincloss",
+    displayName: "Jake Auchincloss",
+    preferredName: "Jake",
+    title: "Representative",
+    party: "Democratic",
+    district: "MA-04",
+    jurisdiction: "United States House of Representatives",
+    active: true,
+    reelectionYear: 2026,
+    pronunciation: "AW-ken-kloss",
+    birthPlace: "Newton, MA",
+    birthday: "1988-01-29",
+    family: "Wife, Michelle Auchincloss; three children",
+    photoUrl: "https://s3.amazonaws.com/fn-document-service/file-by-sha384/332f6fe7639ff4759ea1e8e879eb3e02c429a9ce4fda47854321ebff5b218066dceef5a4f25f21d505794a6deb62fb84",
+    sourceIdentity: {
+      bioguideId: "A000148",
+      fecCandidateId: "H0MA04192",
+      fecPrincipalCommitteeId: "C00721449",
+      policyNotePersonId: "jpQw91U9",
+      policyNoteEntityId: "legislator-10004532",
+      googleKnowledgeGraphMid: "kg:/g/11f0xw2_p4"
+    },
+    sourceEndpoints: {
+      congressSponsoredLegislation: "https://api.congress.gov/v3/member/A000148/sponsored-legislation",
+      congressCosponsoredLegislation: "https://api.congress.gov/v3/member/A000148/cosponsored-legislation",
+      fecReceipts: "https://api.open.fec.gov/v1/schedules/schedule_a/?committee_id=C00721449",
+      fecDisbursements: "https://api.open.fec.gov/v1/schedules/schedule_b/?committee_id=C00721449",
+      fecIndependentExpenditures: "https://api.open.fec.gov/v1/schedules/schedule_e/?candidate_id=H0MA04192",
+      policyNoteSearch: "https://data.policynote.com/v1/people/search?name=Jake%20Auchincloss",
+      googleFactCheckSearch: "https://factchecktools.googleapis.com/v1alpha1/claims:search?query=Jake+Auchincloss"
+    },
+    committees: [
+      {
+        name: "House Committee on Energy and Commerce",
+        source: "PolicyNote"
+      },
+      {
+        name: "House Committee on Transportation and Infrastructure",
+        source: "PolicyNote"
+      },
+      {
+        name: "House Select Committee on the Chinese Communist Party",
+        source: "PolicyNote"
+      }
+    ],
+    contact: {
+      officialWebsite: "https://auchincloss.house.gov",
+      washingtonOfficePhone: "202-225-5931",
+      districtOfficePhone: "508-431-1110",
+      contactForm: "https://auchincloss.house.gov/contact/emailme"
+    },
+    proofStatus: {
+      identityHub: "Verified proof record",
+      congressGov: "Connected",
+      fec: "Connected",
+      policyNote: "Connected",
+      googleFactCheck: "Connected, no matching claims returned in proof query"
+    }
+  }
+];
